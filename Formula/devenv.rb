2df52c6a512998c3d5dcd7f37cbe48a1cc675f26
Liam Clancy (metafeather) <liam.clancy@superawesome.com>
2024-01-18 14:25:44 +0000

    Replace cask with dependency only formula to avoid codesigning

---
+++ Formula/devenv.rb
@@ -0,0 +1,21 @@
+# typed: false
+# frozen_string_literal: true
+
+class Devenv < Formula
+  desc ""
+  homepage ""
+  version "0.2.0"
+
+  # Only has dependencies
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  depends_on "metafeather/tools/devenv@0.2.0-rc"
+
+  def install
+    (bin + "devenv.versions").write <<~EOS
+      #!/bin/sh
+      echo "#{version}"
+    EOS
+  end
+end
